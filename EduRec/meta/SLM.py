66373af419f55db7538c906e906a470093a56f0a
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-03-22 16:28:55 +0800

    [feat] add SLM meta class

---
+++ EduRec/meta/SLM.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# 2021/3/22 @ tongshiwei
+
+class SLM(object):
+    def __init__(self, *args, **kwargs) -> ...:
+        pass
+
+    def train(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
+
+    def eval(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
+
+    def save(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
+
+    def load(self, *args, **kwargs) -> ...:
+        raise NotImplementedError
--- EduRec/meta/__init__.py
+++ EduRec/meta/__init__.py
@@ -2,3 +2,4 @@
 # 2021/2/10 @ tongshiwei
 
 from .MeasurementModel import MeasurementModel
+from .SLM import SLM
